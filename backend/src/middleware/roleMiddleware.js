bbec3f4370840f125ad1c9cac5e0cc416ff74554
tmKla <tm.klajn@gmail.com>
2026-02-14 19:14:26 +0100

    FIX Vulnerabily 4: Insecure JWT Claims Trust in middleware/roleMiddleware.js

--- backend/src/middleware/roleMiddleware.js
+++ backend/src/middleware/roleMiddleware.js
@@ -1,17 +1,20 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 const requireAdmin = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   console.log("Authorization Header:", authHeader);
   const token = authHeader && authHeader.split(' ')[1];
   console.log("Extracted Token:", token);
   if (!token) return res.status(401).json({ error: "No token provided" });
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) return res.status(403).json({ error: "Invalid token" });
-        if (decoded.role === 'ADMIN') {
-            req.user = decoded;
+        const user = await User.findByPk(decoded.id);
+        if (!user) return res.status(404).json({ error: "User not found" });
+        if (user.role === 'ADMIN') {
+            req.user = user;
             next();
         } else {
-            res.status(403).json({ error: "Access denied: Admin role required" });
+            res.status(403).json({ error: "Access denied: Permissions revoked." });
         }
     });
 };
